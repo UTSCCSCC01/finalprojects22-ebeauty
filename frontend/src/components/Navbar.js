4f94e77eba519b0f8582734075da86def67b67cf
Dlontheway <devinfromc@gmail.com>
2022-06-08 04:13:52 +0800

    push rough work to allen's branch, will delete this branch later

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import '../css/index.css';
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import '../css/index.css'
 
 const Navbar = () => {
 
-  let navigate = useNavigate();
+  let navigate = useNavigate()
 
-  function handleClick() {
-    navigate("/");
+  function handleClick () {
+    navigate("/")
   }
 
   return (
@@ -15,12 +15,13 @@ const Navbar = () => {
       <h1 onClick={handleClick}>Amorr</h1>
       <div className="links">
         <Link to="/">Service</Link>
+        <Link to="/searchpage">Search</Link>
         <Link to="/findjob">Find a Job</Link>
         <Link to="/login">Log In</Link>
         <Link to="/signup" className={"toprightbotton"}>Sign Up</Link>
       </div>
     </nav>
-  );
+  )
 }
- 
-export default Navbar;
+
+export default Navbar
---
+++ frontend/src/components/SearchPage.js
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom"
+import '../css/index.css'
+import '../css/SearchPage.css'
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+// create a new component called search page, which will hold the search bar and the results, and then export it, so that it can be used in the main page
+const SearchPage = () => {
+  let navigate = useNavigate()
+  function handleClick () {
+    navigate("/searchpage")
+  }
+  return (
+    <div className="search-page">
+      <div className="search-bar">
+        <div className="search-btn">
+          <button className="search-icon" type="submit" onClick={handleClick}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+        <input type="text" className="search-text" placeholder="No Result? Find Another Service Here!" />
+      </div>
+    </div>
+  )
+}
+
+export default SearchPage
