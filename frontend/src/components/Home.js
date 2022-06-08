d3796cf2425364733fba1b72c7faebc839e1b346
Eddie Shin <eshinhw@gmail.com>
2022-06-08 02:17:25 -0400

    implemented basic functionality of homepage hover images

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,3 @@
-
 const Home = () => {
   return (
     <>
@@ -12,22 +11,68 @@ const Home = () => {
           <h1 className="search-header">
             <span>Find Your Beauty Today</span>
           </h1>
-          <p className="search-body">Find Beauty Professionals with the help of Amor</p>
+          <p className="search-body">
+            Find Beauty Professionals with the help of Amor
+          </p>
           <div className="search-form">
             <div className="sb-container-input">
-              <input type="text" className="sb-input" autoComplete="off" placeholder="I need help with" />
-            <button type="button" className="btn sb-button">Find Beauty Today</button>
+              <input
+                type="text"
+                className="sb-input"
+                autoComplete="off"
+                placeholder="I need help with"
+              />
+              <button type="button" className="btn sb-button">
+                Find Beauty Today
+              </button>
+            </div>
           </div>
         </div>
-        <div className="sb-examples">
-          <button className="btn-secondary">Hair Designer</button>
-          <button className="btn-secondary">Nail Polisher</button>
-          <button className="btn-secondary">Makeup Artist</button>
+      </div>
 
+      {/* <img src={require("../images/haircut.jpg")} alt="haircut" /> */}
+
+      <div className="row">
+        <div className="column">
+          <div class="image">
+            <img
+              class="image__img"
+              src={require("../images/haircut.jpg")}
+              alt="Bricks"
+            />
+            <div class="image__overlay image__overlay--primary">
+              <div class="image__title">Haircut</div>
+              <p class="image__description">Enjoy New Style!</p>
+            </div>
+          </div>
         </div>
+
+        <div className="column">
+          <div className="image">
+            <img
+              className="image__img"
+              src={require("../images/nail-polish.jpg")}
+              alt="Bricks"
+            />
+            <div className="image__overlay image__overlay--primary">
+              <div className="image__title">Nail</div>
+              <p className="image__description">Enjoy New Style!</p>
+            </div>
+          </div>
         </div>
-
-
+        <div className="column">
+          <div className="image">
+            <img
+              className="image__img"
+              src={require("../images/makeup.jpg")}
+              alt="Bricks"
+            />
+            <div className="image__overlay image__overlay--primary">
+              <div className="image__title">Makeup</div>
+              <p className="image__description">Enjoy New Style!</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
