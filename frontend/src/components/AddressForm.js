760e1ddc633b9ae5ee68b7222145214ce046467d
Moonman137 <zhouyz530@gmail.com>
2022-06-08 04:48:53 -0400

    uploaded the basic frame of EB-2 without CSS

---
+++ frontend/src/components/AddressForm.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Button = ({color}) => {
+  const onClick = () => {
+    console.log('address entered')
+    const btn = document.querySelector('#address-btn')
+    btn.style.backgroundColor='pink'
+    setTimeout (()=>btn.style.backgroundColor='grey', 1000)
+    const addr = document.querySelector('#address')
+    const msg1 = document.querySelector('#m1')
+    const msg2 = document.querySelector('#m2')
+    console.log(addr.value)
+    if (addr.value !== ''){
+      msg2.innerHTML = 'address added successfully!'
+      setTimeout (()=>msg2.innerHTML='', 1000)
+    }
+    else{
+      msg1.innerHTML ='Please enter field'
+      setTimeout (()=>msg1.innerHTML='', 1000)
+    }
+  }
+  return (
+    <div>
+      <h2> Enter the Address </h2>
+      <div>
+        <label htmlFor="address"> Address: </label>
+        <div className='message1' id='m1'></div>
+        <input type="text" id='address'/>
+      </div>
+      <button onClick={onClick} style={{backgroundColor: color}} className='btn' id='address-btn'>Enter</button>
+      <div className='message2' id='m2'></div>
+    </div>
+    
+  )
+}
+Button.defaultProps = {
+  color: 'grey'
+}
+export default Button
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+const Header = ({title}) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title:'Amorr',
+}
+Header.propTypes = {
+  title: PropTypes.string
+}
+export default Header
