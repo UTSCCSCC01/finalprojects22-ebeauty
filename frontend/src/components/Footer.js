80914f617e00627d56657158ed8bcda00beb30fc
Eddie Shin <eshinhw@gmail.com>
2022-06-08 14:11:14 -0400

    EB-1 updated footer contents

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
-import '../css/index.css';
+import "../css/index.css";
 
 const Footer = () => {
-
   return (
     // <nav className="footer">
     //   <Link to="/">About Us</Link>
@@ -11,43 +10,47 @@ const Footer = () => {
 
     <div className="footer-container">
       <footer>
-        <div className="footer-social">
-          <div className="footer-social-container">
-            <span>Follow Us!</span>
-            <a>Instagram</a>
-            <a>Facebook</a>
-          </div>
-        </div>
         <div className="footer-content">
           <div className="footer-content-container">
             <div className="footer-content-column">
               <span className="footer-content-title">Discover</span>
               <div className="footer-links">
-                <a>Become a Service Provider</a><br/>
-                <a>Service By City</a><br/>
-                <a>All Services</a><br/>
+                <a>Become a Service Provider</a>
+                <br />
+                <a>Service By City</a>
+                <br />
+                <a>All Services</a>
+                <br />
                 <a>Help</a>
               </div>
             </div>
             <div className="footer-content-column">
               <span className="footer-content-title">Company</span>
               <div className="footer-links">
-                <a>About Us</a><br/>
-                <a>Careers</a><br/>
-                <a>Press</a><br/>
-                <a>Blog</a><br/>
+                <a>About Us</a>
+                <br />
+                <a>Careers</a>
+                <br />
+                <a>Press</a>
+                <br />
+                <a>Blog</a>
+                <br />
                 <a>Terms & Privacy</a>
+              </div>
+            </div>
+            <div className="footer-content-column">
+              <span className="footer-content-title">Follow Us</span>
+              <div className="footer-links">
+                <a>Instagram</a>
+                <br />
+                <a>Facebook</a>
               </div>
             </div>
           </div>
         </div>
-
-
-
       </footer>
-
     </div>
   );
-}
- 
+};
+
 export default Footer;
