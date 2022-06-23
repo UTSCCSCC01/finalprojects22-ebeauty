c3160a64c41841888f2ad99907468d8ee0c5cf3f
Allen <allen.lo@mail.utoronto.ca>
2022-06-23 16:04:13 -0400

    EB 33 add further comments

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,18 @@ app.listen(port, () => {
 })
 
 /**
- * Note: use postman raw sending json object like A1
  * It's just a simple app without creating any user, not connected to frontend as well. 
+ * 
+ * Note: use postman raw sending json object like A1 to: localhost:5000/api/goals
+ *       currently, recording to schema goalModel.js, I need a field named movieId that correspond to string
+ *       so how it works is you select method, input the url, go to body select raw and JSON, 
+ *       if doing the methods that need a body, like post, put, delete. 
+ *       We have body looks like this: 
+{
+"movieId": "1"
+}
  * NOTE: currently we go server.js -> route.js -> controller.js -> goalModel.js to send, get messages from database
  *                                 -> db.js to connect to mongodb
  *                                 -> errorMiddleware.js to handle showing error
- * additional .env is link that help connect to database for db.js process.env.MONGO_URI
+ * additional: .env is link that help connect to database for db.js process.env.MONGO_URI
  */
