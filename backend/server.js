5123cd8d9c2d4b5b15e05bb932657dfa4a1f6b1f
Eddie Shin <eshinhw@gmail.com>
2022-06-25 18:43:01 -0400

    added reviewRoutes for Review DB

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,37 @@
-const express = require('express');
+const express = require("express");
 // used on connecting frontend
-const path = require('path');
-const port = (process.env.PORT) || 5000
+const path = require("path");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
 // handle error and show it inside postman
-const {errorHandler} = require('./middleware/errorMiddleware');
+const { errorHandler } = require("./middleware/errorMiddleware");
 // used on get the .env file
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 // connect the database here
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 connectDB();
+
+
 
 const app = express();
 
 // to retrieve data from postman to be not undefined.
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/posts', require('./routes/postRoute'));
-app.use('/api/providers', require('./routes/providerRoute'));
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+app.use(errorHandler);
+app.use("/api/posts", require("./routes/postRoute"));
+app.use("/api/providers", require("./routes/providerRoute"));
+// app.use("*", (req, res) => res.status(404).json({ error: "No endpoint exists" }));
+
+// Enabled all the review routes in server.js
+const reviewRoutes = require("./routes/reivewRoute");
+app.use("/api/reviews", reviewRoutes);
 
 // Serve frontend
 /*
@@ -29,8 +42,6 @@ app.get('*', (req, res) =>
   )
 )*/
 
-app.use(errorHandler);
-
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-})
+});
