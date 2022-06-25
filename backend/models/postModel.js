104ef925c12d59fc4095fe3f5ebc1aab16907bf2
Eddie Shin <eshinhw@gmail.com>
2022-06-25 15:50:32 -0400

    defined schema for Review DB

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -1,23 +1,51 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const postSchema = mongoose.Schema({
-  provider_name: {
-    type: String, 
-    require: true, 
-  },
-  provider_id: {
-    type: mongoose.Schema.Types.ObjectId, 
-    require: true, 
-    ref: 'Provider'
-  },
-  postText:{
+const postSchema = mongoose.Schema(
+  {
+    provider_name: {
       type: String,
-      required: [true, "please add text."]
-    }
-  }, 
+      require: true,
+    },
+    provider_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "Provider",
+    },
+    postText: {
+      type: String,
+      required: [true, "please add text."],
+    },
+  },
   {
     timestamps: true,
   }
-)
+);
 
-module.exports = mongoose.model('Post',postSchema);
+const reviewSchema = mongoose.Schema(
+  {
+    customer_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "Provider",
+    },
+    provider_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "Provider",
+    },
+    review_title: {
+      type: String,
+      require: true,
+    },
+    review_message: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Post", postSchema);
+module.exports = mongoose.model("Review", reviewSchema); 
