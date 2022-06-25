f7ea0c688d872c340579872a6cde94f6b792bcfe
Eddie Shin <eshinhw@gmail.com>
2022-06-25 15:56:33 -0400

    removed reviewSchema to its own file

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -21,31 +21,5 @@ const postSchema = mongoose.Schema(
   }
 );
 
-const reviewSchema = mongoose.Schema(
-  {
-    customer_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      require: true,
-      ref: "Provider",
-    },
-    provider_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      require: true,
-      ref: "Provider",
-    },
-    review_title: {
-      type: String,
-      require: true,
-    },
-    review_message: {
-      type: String,
-      require: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
 module.exports = mongoose.model("Post", postSchema);
-module.exports = mongoose.model("Review", reviewSchema); 
+
