84a7502f0677b9bd2ee424560d452be60f763d0f
Eddie Shin <eshinhw@gmail.com>
2022-06-30 02:56:35 -0400

    set routes for customers

--- backend/routes/customerRoute.js
+++ backend/routes/customerRoute.js
@@ -1,25 +1,29 @@
 import {
   createCustomer,
   getCustomers,
-  getSingleCustomer,
+  getCustomer,
   deleteCustomer,
   updateCustomer,
+  registerCustomer,
+  loginCustomer,
 } from "../controllers/customerController.js";
 import express from "express";
 
 const router = express.Router();
 
+router.post("/register-customer", registerCustomer);
+router.post("/login-customer", loginCustomer);
+// router.get('/me', protect, getProvider);
+
 // get all reviews
-router.get("/", getCustomers);
+router.get("/customers", getCustomers);
 
 // get single review
-router.get("/:customerId", getSingleCustomer);
-
-router.post("/", createCustomer);
+router.get("/:customerId", getCustomer);
 
 //
-router.delete("/:customerId", deleteCustomer);
+router.delete("/customers/:customerId", deleteCustomer);
 
-router.patch("/:customerId", updateCustomer);
+router.patch("/customers/:customerId", updateCustomer);
 
 export default router;
