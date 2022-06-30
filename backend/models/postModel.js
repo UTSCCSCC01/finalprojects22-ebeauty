209f810b9cc749bb799357dc59e7743631d945fe
Eddie Shin <eshinhw@gmail.com>
2022-06-30 00:05:21 -0400

    resolved the merge conflicts

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema(
@@ -17,34 +16,9 @@ const postSchema = mongoose.Schema(
       required: [true, "please add text."],
     },
   },
-=======
-import mongoose from "mongoose";
-
-const postSchema = mongoose.Schema({
-  provider_name: {
-    type: String,
-    require: true,
-  },
-  provider_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    require: true,
-    ref: 'Provider'
-  },
-  postText: {
-    type: String,
-    required: [true, "please add text."]
-  }
-},
->>>>>>> 64d9256ca352c1fffb78bffdaa2dd3b4de96e5b0
   {
     timestamps: true,
   }
 );
-<<<<<<< HEAD
-
-module.exports = mongoose.model("Post", postSchema);
-
-=======
 
 export default mongoose.model("Post", postSchema);
->>>>>>> 64d9256ca352c1fffb78bffdaa2dd3b4de96e5b0
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-// used on connecting frontend
-const path = require("path");
-const mongoose = require("mongoose");
-const port = process.env.PORT || 3000;
-// handle error and show it inside postman
-const { errorHandler } = require("./middleware/errorMiddleware");
-// used on get the .env file
-const dotenv = require("dotenv").config();
-
-// connect the database here
-const connectDB = require("./config/db");
-=======
 import express from 'express';
 import profiles from './data/profiles.js';
 import colors from 'colors';
@@ -34,7 +20,6 @@ dotenv.config();
 const port = (process.env.PORT) || 5000;
 
 // connect the database here
->>>>>>> 64d9256ca352c1fffb78bffdaa2dd3b4de96e5b0
 connectDB();
 
 
@@ -44,7 +29,6 @@ const app = express();
 // to retrieve data from postman to be not undefined.
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-<<<<<<< HEAD
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -58,13 +42,11 @@ app.use("/api/providers", require("./routes/providerRoute"));
 // Enabled all the review routes in server.js
 const reviewRoutes = require("./routes/reviewRoute");
 app.use("/api/reviews", reviewRoutes);
-=======
 
 // use the routes here
 app.use('/api/posts', posts);
 app.use('/api/providers', providers);
 app.use('/api/taskproviders', taskproviderRoute);
->>>>>>> 64d9256ca352c1fffb78bffdaa2dd3b4de96e5b0
 
 // take frontend content (static asset) to backend. but if you modified frontend, then it needs to re-run npm run build in frontend folder everytime.
 // thus used for deployment in future. if uncomment below, just visit port:500 would show content of port:3000
@@ -80,9 +62,5 @@ app.get('*', (req, res) =>
 );*/
 
 app.listen(port, () => {
-<<<<<<< HEAD
-  console.log(`Server started on port ${port}`);
-=======
   console.log(`Server started under ${process.env.NODE_ENV} on port ${port}`.yellow.bold);
->>>>>>> 64d9256ca352c1fffb78bffdaa2dd3b4de96e5b0
 });
