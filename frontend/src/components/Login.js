a044b6e4a6cdcd91ef2ba3b7f33103e1658c3bc2
Eddie Shin <eshinhw@gmail.com>
2022-06-30 03:25:06 -0400

    connecting frontend to backend/DB

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,58 +1,87 @@
-import { useState } from 'react';
-import '../css/Login.css'
+import { useState } from "react";
+import "../css/Login.css";
 
 const Login = () => {
-    const [data, setData] = useState({
-        email:"",
-        password:""
-    })
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleChange = ({currentTarget: input}) => {
-        setData({...data, [input.name]: input.value});
-    };
+  // const [data, setData] = useState({
+  //   email: "",
+  //   password: "",
+  // });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  // const handleChange = ({ currentTarget: input }) => {
+  //   setData({ ...data, [input.name]: input.value });
+  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const customer = { email, password };
+
+    const response = await fetch("/api/login-customer/", {
+      method: "POST",
+      body: JSON.stringify(customer),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
     }
 
-    return (
-        <div className={"login_container"} >
-            <div className={"login_form_container"}>
-                <div className={"left"}>
-                <form className={"form_container"}>
-                    <h1 className={"form_container h1"}>Login to Your Account</h1>
-                    <input 
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        onChange={handleChange}
-                        value={data.email}
-                        requiredclassName={"form_container input"}
-                    />
-                    <input 
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        onChange={handleChange}
-                        value={data.password}
-                        requiredclassName={"form_container input"}
-                    />
-                    <button type="submit" className={"green_btn"}>
-                        Login
-                    </button>
-                </form>
-            </div>
-            <div className={"right"}>
-                <h1>New Here?</h1>
-                    <a href = "/signup">
-                        <button type='button' className={"white_btn"}>
-                            Sign Up
-                        </button>
-                    </a>
-                </div>
-            </div>
+    if (response.ok) {
+      setError(null);
+      setEmail("");
+      setPassword("");
+    }
+  };
+
+  return (
+    <div className="login_container">
+      <div className="login_form_container">
+        <div className="left">
+          <form className="form_container">
+            <h1 className="form_container h1">Login to Your Account</h1>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
+              requiredclassName="form_container input"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
+              requiredclassName="form_container input"
+            />
+            <button type="submit" className="green_btn" onClick={handleSubmit}>
+              Login
+            </button>
+          </form>
         </div>
-    );
-}
- 
+        <div className="right">
+          <h1>New Here?</h1>
+          <a href="/signup">
+            <button type="button" className="white_btn">
+              Sign Up
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -2,75 +2,112 @@ import { useState } from 'react';
 import '../css/SignUp.css'
 
 const SignUp = () => {
-    const [data, setData] = useState({
-        firstName: "",
-        lastName:"",
-        email:"",
-        password:""
-    })
+  const [error, setError] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleChange = ({currentTarget: input}) => {
-        setData({...data, [input.name]: input.value});
-    };
+  // const [data, setData] = useState({
+  //     firstName: "",
+  //     lastName:"",
+  //     email:"",
+  //     password:""
+  // })
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  // const handleChange = ({currentTarget: input}) => {
+  //     setData({...data, [input.name]: input.value});
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const customer = { firstName, lastName, email, password };
+
+    const response = await fetch("/api/register-customer/", {
+      method: "POST",
+      body: JSON.stringify(customer),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
     }
 
-    return (
-        <div className={"signup_container"} >
-            <div className={"signup_form_container"}>
-                <div className={"leftsignup"}>
-                    <h1>Welcome Back</h1>
-                    <a href = "/login">
-                        <button type='button' className={"white_btnsignup"}>
-                            Sign In
-                        </button>
-                    </a>
-                </div>
-                <div className={"rightsignup"}>
-                    <form className={"form_containersignup"}>
-                        <h1 className={"form_containersignup h1"}>Create Account</h1>
-                        <input 
-                            type="text"
-                            placeholder="First Name"
-                            name="firstName"
-                            onChange={handleChange}
-                            value={data.firstName}
-                            requiredclassname={"form_containersignup input"}
-                        />
-                        <input 
-                            type="text"
-                            placeholder="Last Name"
-                            name="lastName"
-                            onChange={handleChange}
-                            value={data.lastName}
-                            requiredclassname={"form_containersignup input"}
-                        />
-                        <input 
-                            type="email"
-                            placeholder="Email"
-                            name="email"
-                            onChange={handleChange}
-                            value={data.email}
-                            requiredclassname={"form_containersignup input"}
-                        />
-                        <input 
-                            type="password"
-                            placeholder="Password"
-                            name="password"
-                            onChange={handleChange}
-                            value={data.password}
-                            requiredclassname={"form_containersignup input"}
-                        />
-                        <button type="submit" className={"green_btnsignup"}>
-                            Sign Up
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+    if (response.ok) {
+      setError(null);
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPassword("");
+    }
+  }
+
+  return (
+      <div className={"signup_container"} >
+          <div className={"signup_form_container"}>
+              <div className={"leftsignup"}>
+                  <h1>Welcome Back</h1>
+                  <a href = "/login">
+                      <button type='button' className={"white_btnsignup"}>
+                          Sign In
+                      </button>
+                  </a>
+              </div>
+              <div className={"rightsignup"}>
+                  <form className={"form_containersignup"}>
+                      <h1 className={"form_containersignup h1"}>Create Account</h1>
+                      <input 
+                          type="text"
+                          placeholder="First Name"
+                          name="firstName"
+                          onChange={(e) => {
+                            setFirstName(e.target.value);
+                          }}
+                          value={firstName}
+                          requiredclassname={"form_containersignup input"}
+                      />
+                      <input 
+                          type="text"
+                          placeholder="Last Name"
+                          name="lastName"
+                          onChange={(e) => {
+                            setLastName(e.target.value);
+                          }}
+                          value={lastName}
+                          requiredclassname={"form_containersignup input"}
+                      />
+                      <input 
+                          type="email"
+                          placeholder="Email"
+                          name="email"
+                          onChange={(e) => {
+                            setEmail(e.target.value);
+                          }}
+                          value={email}
+                          requiredclassname={"form_containersignup input"}
+                      />
+                      <input 
+                          type="password"
+                          placeholder="Password"
+                          name="password"
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                          value={password}
+                          requiredclassname={"form_containersignup input"}
+                      />
+                      <button type="submit" className={"green_btnsignup"} onClick={handleSubmit}>
+                          Sign Up
+                      </button>
+                  </form>
+              </div>
+          </div>
+      </div>
+  );
 }
  
 export default SignUp;
