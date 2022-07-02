7f664360819d39ebb6e2b8cbe95be88b27a5d6dc
Eddie Shin <eshinhw@gmail.com>
2022-07-02 17:01:35 -0400

    implemented star rating with material UI

--- frontend/src/pages/ReviewPage.js
+++ frontend/src/pages/ReviewPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { FaStar } from "react-icons/fa";
 import "../css/Review.css";
+import { Rating } from '@mui/material';
 
 const Review = () => {
   const [error, setError] = useState(null);
@@ -41,29 +41,15 @@ const Review = () => {
       <div className="review-container">
         <h1 className="title">How do you like the service today?</h1>
         <div className="star-rating">
-          {[...Array(5)].map((star, i) => {
-            const ratingValue = i + 1;
-            console.log("ratingValue: ", ratingValue);
-            console.log("rating: ", rating);
-
-            return (
-              <label key={i}>
-                <input
-                  type="radio"
-                  name="rating"
-                  value={ratingValue}
-                  onClick={() => setRating(ratingValue)}
-                />
-                <FaStar
-                  className="star"
-                  color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e539"}
-                  size={60}
-                  onMouseEnter={() => setHover(ratingValue)}
-                  onMouseLeave={() => setHover(null)}
-                />
-              </label>
-            );
-          })}
+          <Rating
+            name="simple-controlled"
+            value={rating}
+            onChange={(e, rating) => {
+              console.log(rating);
+              setRating(rating);
+            }}
+            size="large"
+          />
         </div>
         <form className="add-review" onSubmit={handleSubmit}>
           <textarea
