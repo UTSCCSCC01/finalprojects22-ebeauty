eb07991f070951c378dab74af5487d62e528e4be
Moonman137 <zhouyz530@gmail.com>
2022-07-02 21:13:07 -0400

    modified the backend to get user's address in db

--- backend/routes/customerRoute.js
+++ backend/routes/customerRoute.js
@@ -17,33 +17,10 @@ import {
 
 } from "../controllers/customerController.js";
 import express from "express";
-
-// checks if customer is logged in, if so store user info in res.locals.customer
-const checkUser = (req, res, next) => {
-  const token = req.cookies.jwt;
-  console.log(token);
-  if (token){
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decodedToken) => {
-      if (err){
-        console.log(err.message);
-        res.locals.customer = null;
-        next();
-      }
-      else{
-        console.log(decodedToken);
-        let customer = await Customer.findById(decodedToken.id);
-        res.locals.customer = customer;
-        next();
-      }
-    })
-  }
-  else{
-    res.locals.customer = null;
-    next();
-  }
-}
+import cookieParser from "cookie-parser";
 
 const router = express.Router();
+router.use(cookieParser());
 
 router.post("/register-customer", registerCustomer);
 router.post("/login-customer", loginCustomer);
@@ -52,15 +29,7 @@ router.post("/login-customer", loginCustomer);
 // get all reviews
 router.get("/", getCustomers);
 
-// get single review
-router.get("/:customerId", getCustomer);
-
-router.delete("/:customerId", deleteCustomer);
-
-router.patch("/:customerId", updateCustomer);
-
-router.get("/getDefaultAddress", checkUser);
-// router.get("/getDefaultAddress", checkUser);
+router.get("/getDefaultAddress", getDefaultAddress);
 // router.get("/getAllAddress", getAllAddress);
 
 // router.patch("/deleteAddress1", deleteAddress1);
@@ -69,6 +38,13 @@ router.get("/getDefaultAddress", checkUser);
 // router.patch("/updateDefaultAddress", updateDefaultAddress);
 // router.patch("/updateAddress1", updateAddress1);
 // router.patch("/updateAddress2", updateAddress2);
+
+// get single review
+router.get("/:customerId", getCustomer);
+
+router.delete("/:customerId", deleteCustomer);
+
+router.patch("/:customerId", updateCustomer);
 
 
 export default router;
