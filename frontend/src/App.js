348d9e86511df6759ca85cfbdaae03a3575292ca
Eddie Shin <eshinhw@gmail.com>
2022-07-13 01:07:47 -0400

    implemented checkout input validation for credit card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,33 @@
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/HomePage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './css/components.css';
-import Signup from './pages/SignUp';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import ScrollToTop from './components/ScrollToTop';
-import FindJob from './components/FindJob';
-import SearchPage from './components/SearchPage';
-import SignUpProvider from './components/SignUpProvider';
-import SignUpProviderTwo from './components/SignUpProviderTwo';
-import ProfileProvider from './components/ProfileProvider';
-import ViewProfileProvider from './components/ViewProfileProvider';
-import AddressPage from './components/AddressPage';
-import Review from './pages/Review';
-import CheckoutMain from './pages/CheckoutMain'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./css/components.css";
+import Signup from "./pages/SignUp";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
+import FindJob from "./pages/FindJob";
+import SearchPage from "./pages/SearchPage";
+import SignUpProvider from "./pages/SignUpProvider";
+import SignUpProviderTwo from "./pages/SignUpProviderTwo";
+import SignUpProviderThree from "./pages/SignUpProviderThree";
+import ProfileProvider from "./pages/ProfileProvider";
+import ViewProfileProvider from "./pages/ViewProfileProvider";
+import AddressPage from "./pages/AddressPage";
+import Review from "./pages/ReviewPage";
+// import CheckoutMain from "./pages/CheckoutMain";
 //react-notifications-component
-import { ReactNotifications } from 'react-notifications-component'
-import 'react-notifications-component/dist/theme.css'
+import { ReactNotifications } from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
+import CheckoutAddress from "./pages/CheckoutAddress";
+import CheckoutPayment from "./pages/CheckoutPayment";
+import CheckoutReview from "./pages/CheckoutReview";
 
 function App() {
   return (
     <Router>
-      <div className="App" >
+      <div className="App">
         <ReactNotifications />
         <ScrollToTop>
           <div className="content">
@@ -32,14 +36,10 @@ function App() {
               <Route path="*" element={<NotFound />} />
               <Route exact path="/" element={<Home />} />
               <Route path="/reviews" element={<Review />} />
-              <Route path="/checkout" element={<CheckoutMain />} />
               <Route path="/searchpage" element={<SearchPage />} />
               <Route path="/page/:pageNumber" element={<SearchPage />} />
               <Route path="/searchpage/:keyword" element={<SearchPage />} />
-              <Route
-                path="/searchpage/:keyword/page/:pageNumber"
-                element={<SearchPage />}
-              />
+              <Route path="/searchpage/:keyword/page/:pageNumber" element={<SearchPage />} />
               <Route path="/findjob" element={<FindJob />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/signupproviderone" element={<SignUpProvider />} />
@@ -47,11 +47,11 @@ function App() {
               <Route path="/signupproviderthree" element={<SignUpProviderThree />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profileprovider" element={<ProfileProvider />} />
-              <Route
-                path="/taskprovider/:id"
-                element={<ViewProfileProvider />}
-              />
+              <Route path="/taskprovider/:id" element={<ViewProfileProvider />} />
               <Route path="/addresspage" element={<AddressPage />} />
+              <Route path="/checkout-address" element={<CheckoutAddress />} />
+              <Route path="/checkout-payment" element={<CheckoutPayment />} />
+              <Route path="/checkout-review" element={<CheckoutReview />} />
             </Routes>
             <Footer />
           </div>
--- frontend/src/pages/CheckoutReview.js
+++ frontend/src/pages/CheckoutReview.js
@@ -1,83 +1,124 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+import { Box, Button, Container, createTheme, Paper, ThemeProvider } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
 
 const products = [
   {
-    name: 'Amorr Salon',
-    desc: 'Man\'s Haircut booked on July 23, 2022',
-    price: '$18.99',
+    name: "Amorr Salon",
+    desc: "Man's Haircut booked on July 23, 2022",
+    price: "$18.99",
   },
   {
-    name: 'Fresh Massage',
-    desc: 'Woman\'s Manicure booked on July 25, 2022',
-    price: '$21.99',
+    name: "Fresh Massage",
+    desc: "Woman's Manicure booked on July 25, 2022",
+    price: "$21.99",
   },
   {
-    name: 'Lily Makeup Shop',
-    desc: 'Woman\s Makeup booked on August 5, 2022',
-    price: '$45.99',
-  }
+    name: "Lily Makeup Shop",
+    desc: "Womans Makeup booked on August 5, 2022",
+    price: "$45.99",
+  },
 ];
 
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
-const payments = [
-  { name: 'Card type', detail: 'Visa' },
-  { name: 'Card holder', detail: 'Mr John Smith' },
-  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-  { name: 'Expiry date', detail: '04/2024' },
-];
+const handleSubmitOrder = () => {
+  // await fetch("/api/orders/", {
+  //   method: "POST",
+  //   body: JSON.stringify(order),
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // }).then((res) => {});
+};
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#e27b7b",
+    },
+    secondary: {
+      main: "#e27b7b",
+    },
+  },
+});
 
 export default function CheckoutReview() {
+  const location = useLocation();
+  const data = location.state.data;
+  const addresses = [data.address.addressOne, data.address.addressOne, data.address.city, data.address.postalCode, data.address.country];
+  const payments = [
+    { name: "Card holder", detail: data.payment.nameOnCard },
+    { name: "Card number", detail: data.payment.cardNumber },
+    { name: "Expiry date", detail: `${data.payment.expiryMonth}/${data.payment.expiryYear}` },
+  ];
   return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Order summary
-      </Typography>
-      <List disablePadding>
-        {products.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
-          </ListItem>
-        ))}
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+              Order summary
+            </Typography>
+            <List disablePadding>
+              {products.map((product) => (
+                <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+                  <ListItemText primary={product.name} secondary={product.desc} />
+                  <Typography variant="body2">{product.price}</Typography>
+                </ListItem>
+              ))}
 
-        <ListItem sx={{ py: 1, px: 0 }}>
-          <ListItemText primary="Total" />
-          <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $86.97
-          </Typography>
-        </ListItem>
-      </List>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Service Address
-          </Typography>
-          <Typography gutterBottom>John Smith</Typography>
-          <Typography gutterBottom>{addresses.join(', ')}</Typography>
-        </Grid>
-        <Grid item container direction="column" xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Payment details
-          </Typography>
-          <Grid container>
-            {payments.map((payment) => (
-              <React.Fragment key={payment.name}>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.name}</Typography>
+              <ListItem sx={{ py: 1, px: 0 }}>
+                <ListItemText primary="Total" />
+                <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+                  $86.97
+                </Typography>
+              </ListItem>
+            </List>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+                  Service Address
+                </Typography>
+                <Typography gutterBottom>{data.firstName} {data.lastName} </Typography>
+                <Typography gutterBottom>{addresses.join(", ")}</Typography>
+              </Grid>
+              <Grid item container direction="column" xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+                  Payment details
+                </Typography>
+                <Grid container>
+                  {payments.map((payment) => (
+                    <React.Fragment key={payment.name}>
+                      <Grid item xs={6}>
+                        <Typography gutterBottom>{payment.name}</Typography>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <Typography gutterBottom>{payment.detail}</Typography>
+                      </Grid>
+                    </React.Fragment>
+                  ))}
                 </Grid>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.detail}</Typography>
-                </Grid>
-              </React.Fragment>
-            ))}
-          </Grid>
-        </Grid>
-      </Grid>
-    </React.Fragment>
+              </Grid>
+            </Grid>
+            <React.Fragment>
+              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button
+                  className="checkout"
+                  variant="contained"
+                  sx={{ mt: 3, ml: 1 }}
+                  style={{ color: "white" }}
+                >
+                  Order
+                </Button>
+              </Box>
+            </React.Fragment>
+          </React.Fragment>
+        </Paper>
+      </Container>
+    </ThemeProvider>
   );
 }
