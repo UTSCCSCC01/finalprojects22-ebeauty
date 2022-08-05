e6c60a46a752ca486d8d6f93aca9203b21808240
Eddie Shin <eshinhw@gmail.com>
2022-08-04 22:06:40 -0400

    created SignUpGuidePage to direct users in different sign up pages

---
+++ frontend/src/pages/SignUpGuidePage.js
@@ -0,0 +1,65 @@
+// import Grid from "@material-ui/core/Grid";
+
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function SignUpGuidePage() {
+
+  const navigate = useNavigate();
+
+  const customerSignUp = () => {
+    navigate("/signup");
+  }
+  const providerSignUp = () => {
+    navigate("/signupproviderone")
+  }
+  return (
+    <>
+      <Typography align="center" variant="h2" mb={10} mt={10}>
+        Welcome to Amorr!
+      </Typography>
+      <Container>
+        <Grid container>
+          <Grid item xs={6} textAlign="center">
+            <Typography variant="h5">Are you a customer?</Typography>
+            <Button
+              variant="contained"
+              sx={{
+                marginBottom: "50px",
+                marginTop: "50px",
+                backgroundColor: "#e27b7b",
+                color: "white",
+                ":hover": {
+                  bgcolor: "rgb(218, 82, 105)",
+                  color: "white",
+                },
+              }}
+              onClick={customerSignUp}
+            >
+              Sign Up Here
+            </Button>
+          </Grid>
+          <Grid item xs={6} textAlign="center">
+            <Typography variant="h5">Are you a beauty expert?</Typography>
+            <Button
+              variant="contained"
+              sx={{
+                marginBottom: "50px",
+                marginTop: "50px",
+                backgroundColor: "#e27b7b",
+                color: "white",
+                ":hover": {
+                  bgcolor: "rgb(218, 82, 105)",
+                  color: "white",
+                },
+              }}
+              onClick={providerSignUp}
+            >
+              Sign Up Here
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
