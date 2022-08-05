692ee9e589232ecc1e90ff01d94a5635bcad2122
Junxun Liu <devinfromc@gmail.com>
2022-08-05 15:36:51 +0800

    complete search page, dropdown will affect search result

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -10,10 +10,10 @@ import Message from '../components/Message';
 import ProviderCard from '../components/ProviderCard';
 import SearchBox from '../components/SearchBox';
 import Paginator from '../components/Paginator';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { TextField } from '@mui/material'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TextField } from '@mui/material';
 
 // create a new component called search page, which will hold the search bar and the results, and then export it, so that it can be used in the main page
 const SearchPage = () => {
@@ -25,43 +25,48 @@ const SearchPage = () => {
 
   const { keyword } = useParams();
   const { pageNumber } = useParams();
-  console.log(pageNumber);
-  console.log (keyword);
+  const [service, setService] = useState('');
+  // console.log(pageNumber);
+  // console.log(keyword);
   // use dispatch to call the listProviders action
   const dispatch = useDispatch();
   const providersList = useSelector((state) => state.providers);
   const { loading, error, providers, pages, page } = providersList;
-  console.log(pages);
+  // console.log(pages);
   useEffect(() => {
     dispatch(listProviders(keyword, pageNumber));
   }, [dispatch, keyword, pageNumber]);
+  const [date, setDate] = useState('');
+  const [addr, setAddr] = useState('');
 
-  const [date, setDate] = useState("");
-  const [service, setService] = useState("");
-  const [addr,setAddr] = useState("");
-
-  
   useEffect(() => {
-    async function fetchAddr(){
-      await fetch("/api/customers/getDefaultAddress/", {
-        method: "GET",
+    async function fetchAddr() {
+      await fetch('/api/customers/getDefaultAddress/', {
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-      }).then(res => {
-        return res.json()
-      }).then(data => {
-        setAddr(data.address)
       })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setAddr(data.address);
+        });
     }
-    fetchAddr()
-  }, [addr])
+    fetchAddr();
+  }, [addr]);
+
+  const handleServiceChange = (service) => {
+    setService(service);
+    window.location.href = `/searchpage/${service}`;
+  };
 
   return (
     <>
       <div className="search-page">
-        <SearchBox />
-        <div className='address-form'>
+        <SearchBox providers={providers} service={service} key={service._id} />
+        <div className="address-form">
           <h2>{addr}</h2>
           <button onClick={redirect_to_addresspage} className="btn">
             Not where you are?
@@ -71,28 +76,28 @@ const SearchPage = () => {
           <div className="filter-interior-div">
             <h2>Service Type</h2>
             <div className="filter-interior-dropdown-div">
-              <ServiceDropdown 
+              <ServiceDropdown
                 name="service"
-                onChange={(e) =>{
-                  setService(e.target.value);
-                }}
                 value={service}
+                onChange={handleServiceChange}
               />
             </div>
           </div>
           <div className="filter-interior-div">
             <h2>Time and Date</h2>
-              <div className="filter-interior-time-div">
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DatePicker
-                    value={date}
-                    onChange={(newValue) => {
-                      setDate(newValue);
-                    }}
-                    renderInput={(params) => <TextField size="small" {...params} />}
-                  />
-                </LocalizationProvider>
-              </div>
+            <div className="filter-interior-time-div">
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  value={date}
+                  onChange={(newValue) => {
+                    setDate(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField size="small" {...params} />
+                  )}
+                />
+              </LocalizationProvider>
+            </div>
           </div>
         </div>
 
@@ -107,12 +112,7 @@ const SearchPage = () => {
           ) : (
             <div>
               {providers.map((provider) => {
-                return (
-                  <ProviderCard
-                    provider={provider}
-                    key={provider._id}
-                  />
-                );
+                return <ProviderCard provider={provider} key={provider._id} />;
               })}
             </div>
           )}
